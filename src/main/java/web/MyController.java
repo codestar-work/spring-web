2a9c96551170abcd2cc965b5819da0aa08852bec
beer <beer@beers-MacBook-Air.local>
2015-11-12 07:31:22 +0700

    Update

--- src/main/java/web/MyController.java
+++ src/main/java/web/MyController.java
@@ -6,14 +6,10 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MyController {
-	
+
 	@RequestMapping("/")
 	String index() {
 		return "index";
 	}
-	
-	@RequestMapping("/jsp")
-	String jsp() {
-		return "index-jsp";
-	}
+
 }
